0a427daf15b57c108adb2c83531df7b23fb65281
oparabenjamin <oparaben1@gmail.com>
2020-05-06 22:56:02 +0100

    added margin to some text views

--- app/src/main/java/com/benjamin/hookup4u/MainActivity.java
+++ app/src/main/java/com/benjamin/hookup4u/MainActivity.java
@@ -18,6 +18,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void run() {
                 startActivity(new Intent(MainActivity.this, LoginActivity.class));
+                finish();
             }
         }, 3000);
 
