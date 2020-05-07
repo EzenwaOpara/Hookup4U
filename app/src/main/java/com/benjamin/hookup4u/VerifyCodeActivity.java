224ebfeeae7eab06c5e5c3ab3d40aa8eb1581d1b
oparabenjamin <oparaben1@gmail.com>
2020-05-07 16:24:34 +0100

    Added text watcher on the OTP phone verification

--- app/src/main/java/com/benjamin/hookup4u/VerifyCodeActivity.java
+++ app/src/main/java/com/benjamin/hookup4u/VerifyCodeActivity.java
@@ -3,21 +3,84 @@ package com.benjamin.hookup4u;
 import android.app.Dialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class VerifyCodeActivity extends AppCompatActivity {
+
+    private EditText otp1;
+    private EditText otp2;
+    private EditText otp3;
+    private EditText otp4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_verify_code);
+
+        otp1 = findViewById(R.id.otp1);
+        otp2 = findViewById(R.id.otp2);
+        otp3 = findViewById(R.id.otp3);
+        otp4 = findViewById(R.id.otp4);
+
+        otp1.addTextChangedListener(new GenericTextWatcher(otp1));
+        otp2.addTextChangedListener(new GenericTextWatcher(otp2));
+        otp3.addTextChangedListener(new GenericTextWatcher(otp3));
+        otp4.addTextChangedListener(new GenericTextWatcher(otp4));
     }
 
     public void verifyDialog(View view) {
         Dialog dialog = new Dialog(this);
         dialog.setContentView(R.layout.dialog);
         dialog.show();
+    }
+
+    private class GenericTextWatcher implements TextWatcher{
+        private View mView;
+
+        public GenericTextWatcher(View view) {
+            mView = view;
+        }
+
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+        }
+
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+        }
+
+        @Override
+        public void afterTextChanged(Editable editable) {
+            String text = editable.toString();
+            switch (mView.getId()){
+                case R.id.otp1:
+                    if (text.length() == 1)
+                        otp2.requestFocus();
+                    break;
+                case R.id.otp2:
+                    if (text.length() == 1)
+                        otp3.requestFocus();
+                    else if (text.length() == 0)
+                        otp1.requestFocus();
+                    break;
+                case R.id.otp3:
+                    if (text.length() == 1)
+                        otp4.requestFocus();
+                    else if (text.length() == 0)
+                        otp2.requestFocus();
+                    break;
+                case R.id.otp4:
+                    if (text.length() == 0)
+                        otp3.requestFocus();
+                    break;
+            }
+        }
     }
 }
